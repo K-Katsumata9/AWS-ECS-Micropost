b2bfcd6d760b8827c9cc0922fb6cd24a8c2b9cf9
K-Katsumata9 <128912619+K-Katsumata9@users.noreply.github.com>
2024-02-09 19:55:23 +0900

    Delete Micropost directory

--- Micropost/db/migrate/20230926130519_removebirthday_from_users.rb
+++
@@ -1,4 +0,0 @@
-class RemovebirthdayFromUsers < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
--- Micropost/db/migrate/20230926130759_remove_birthday_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBirthdayFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :birthday, :datetime
-  end
-end
